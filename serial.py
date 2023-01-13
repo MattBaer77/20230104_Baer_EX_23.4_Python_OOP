b7ce21e47aad7e67119ab5920bb871784f6cd5b2
Matthew Baer <matt.baer77@gmail.com>
2023-01-13 01:14:59 -0500

    exercise completed

--- serial.py
+++ serial.py
@@ -20,3 +20,19 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        self.start = start
+        self.increment = start
+
+    def __repr__(self):
+        """Show representation"""
+        return f"<SerialGenerator start={self.start} next={self.increment}>"
+
+    def generate(self):
+        before_increment = self.increment
+        self.increment += 1
+        return before_increment
+
+    def reset(self):
+        self.increment = self.start
+
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,65 @@
-"""Word Finder: finds random words from a dictionary."""
-
+from random import choice
 
 class WordFinder:
-    ...
+
+    """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        """Creates a new instance of WordFinder
+
+        Attributes
+        ----------
+        path: str
+            the name of any .txt file containing 1 word per line for at least 1 line
+        words: list
+            a list of the words read from the file from the entered path
+        length: int
+            an integer representing the number of words read
+        
+        """
+
+        self.path = path
+
+        self.words = self.load_dictionary()
+
+        self.length = len(self.words)
+
+    def load_dictionary(self):
+        #reads dictionary file
+        #saves to list
+        words_list = list()
+        words_read = 0
+
+        file = open(f'{self.path}', 'r')
+        for line in file:
+            words_list.append(line.strip())
+            words_read += 1
+        file.close()
+
+        print(f"{words_read} words read.")
+
+        return words_list
+
+    def random(self):
+        """returns a random word from the list of words"""
+        return choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    """Special wordfinder for handling lists with blanks and comments"""
+
+    def load_dictionary(self):
+                #reads dictionary file
+        #saves to list
+        words_list = list()
+        words_read = 0
+
+        file = open(f'{self.path}', 'r')
+        for line in file:
+            if not line.startswith('#'):
+                words_list.append(line.strip())
+                words_read += 1
+        file.close()
+
+        print(f"{words_read} words read.")
+
+        return words_list
